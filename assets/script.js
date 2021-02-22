4d532d473f3fb71554a6304890cbc5b805a914bd
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-02-21 17:16:28 -0800

    took out all the number and checkbox inputs in the html; deleted the variables associsiated with the inputs that were removed; created prompt for how many characters they want and turned that string into an integer; added if statement for when the user plugs in an incorrect value; created 4 confirms for the 4 criteria the user can choose from; added an if statement for if the user doesn't choose any of them; put console.logs in so I can see whether what I have plugged in is working and they are.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var message = document.getElementsByTagName("textarea");
+
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lower = upper.toLowerCase;
 var numbers = "0123456789";
 var specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-
-var charNumber = document.querySelector("#characters");
-var upBox = document.querySelector("#up");
-var lowBox = document.querySelector("#low");
-var numBox = document.querySelector("#num");
-var specialBox = document.querySelector("#special");
 
 // Write password to the #password input
 function writePassword() {
@@ -20,10 +16,41 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
+    var passwordLength = prompt("Please enter the number of characters you want in your password. It must have at least 8 and no more than 128.");
 
+    var length = parseInt(passwordLength);
+    console.log(length);
+
+    if (passwordLength < 8 || passwordLength > 128) {
+      console.log(passwordLength);
+      generatePassword();
+    };
+    var upperCase = confirm("Would you like upper case letters in your password?");
+    var lowerCase = confirm("Would you like lower case letters in your password?");
+    var number = confirm("Would you like numbers in your password?");
+    var special = confirm("Would you like special characters in your password?");
+
+    if (!upperCase && !lowerCase && !number && !special) {
+      var error = confirm("You must pick at least one criteria to create your password (uppercase, lowercase, numbers, or special.");
+      if (error) {
+        generatePassword();
+      }
+    };
+
+    if (upperCase) {
+        console.log(upperCase);
+    };
+    if (lowerCase) {
+        console.log(lowerCase);
+    }; 
+    if (number) {
+        console.log(number);
+    }; 
+    if (special) {
+        console.log(special);
+    };
 }
